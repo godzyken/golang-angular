9d1e2dc29d00ca2a2573b435bb12de850de85bc9
godzy <isgodzy@msn.com>
2020-03-12 13:11:42 +0100

    Phase test de Configuration Mongodb
    Signed-off-by: godzy <isgodzy@msn.com>

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/godzyken/golang-angular/models"
-	"github.com/godzyken/golang-angular/todo"
+	"golang-angular/models"
+	"golang-angular/todo"
 	_ "gopkg.in/mgo.v2"
 	_ "gopkg.in/mgo.v2/bson"
 	"io"
